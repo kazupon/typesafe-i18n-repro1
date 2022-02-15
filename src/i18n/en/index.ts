6cb7a720acbc2ef1c7ac47a4aaddb55388a9fe95
kazuya kawaguchi <kawakazu80@gmail.com>
2022-02-15 15:14:16 +0900

    commit first

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,12 @@
+import type { BaseTranslation } from '../i18n-types.js'
+
+const en: BaseTranslation = {
+	// TODO: your translations go here
+	'HI': 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
+	'*': {
+		'FOO': 'FOO'
+	},
+	'abcdefc/ghijklm': 'abcdefc/ghijklm'
+}
+
+export default en
